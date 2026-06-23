a87520deb62d8af3e0082094dc5f738db0cb8455
Станіслав Новгородський <stanislav@MacBook-Air-Stanislav.local>
2026-06-23 19:41:25 +0300

    refactor(parser): replace prints with logging

--- temp_parser.py
+++ temp_parser.py
@@ -1,11 +1,22 @@
 import requests
 import csv
+import logging
+import sys
 from bs4 import BeautifulSoup, Tag
 from urllib.parse import urljoin
 from fake_useragent import UserAgent
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 from dataclasses import dataclass, fields, astuple
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s]: %(message)s",
+    handlers=[
+        logging.FileHandler("parser.log", encoding="utf-8"),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
 
 @dataclass
 class Product:
@@ -73,10 +84,10 @@ def get_home_products() -> list[Product]:
         return [parse_single_product(product) for product in products]
 
     except requests.exceptions.RequestException as e:
-        print(f"❌ Error while executing the query: {e}")
+        logging.error(f"❌ Error while executing the query: {e}")
         return None
     except Exception as e:
-        print(f"⚠️ Unexpected error: {e}")
+        logging.warning(f"⚠️ Unexpected error: {e}")
         return None
 
 def parse_single_product(product: Tag) -> Product:
@@ -103,11 +114,11 @@ def get_laptop_page_products() -> list[Product]:
         all_products = get_single_page_products(first_page_soup)
 
         num_pages = get_num_pages(first_page_soup)
-        print(f"Number of pages with products: {num_pages} ")
-        print(f"Processing page 1 of {num_pages}")
+        logging.info(f"Total number of pages found: {num_pages}")
+        logging.info(f"Start parsing page 1 from: {len(all_products)}")
 
         for page_num in range(2, num_pages + 1):
-            print(f"Editing the page {page_num} with {num_pages}")
+            logging.info(f"Start parsing the page {page_num} of {num_pages}")
 
             response = session.get(
                 LAPTOP_URL,
@@ -121,7 +132,7 @@ def get_laptop_page_products() -> list[Product]:
             next_page_soup = BeautifulSoup(response.content, 'html.parser')
             all_products.extend(get_single_page_products(next_page_soup))
 
-        print(f"Number of items: {len(all_products)}")
+        logging.info(f"Total number of items found: {len(all_products)}")
         return all_products
 
     except requests.exceptions.RequestException as e:
@@ -160,9 +171,9 @@ def main():
         print("✅ Data successfully saved to the 'products.csv'")
 
     except KeyboardInterrupt:
-        print("\n🛑 The user has terminated the program")
+        logging.warning("\n🛑 The user has terminated the program")
     except Exception as e:
-        print(f"❌ Critical error: {e}")
+        logging.critical(f"❌ Critical error: {e}")
     finally:
         session.close()
 
